2060a0ace3093f435bd3bdc2f1f39a36880417a8
Damir Mingazov <damirm@list.ru>
2018-11-03 12:11:48 +0400

    Initial commit

---
+++ tracktime/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#
+# An application that allows you to entry time in the Redmine throught a bot in Telegram
+"""An application that allows you to entry time in the Redmine throught a bot in Telegram."""
+
+__author__ = 'akanit@tutanota.com'
---
+++ tracktime/__main__.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python
+#
+# An application that allows you to entry time in the Redmine throught a bot in Telegram
+"""An application that allows you to entry time in the Redmine throught a bot in Telegram."""
+
+
+def main():
+    """Example."""
+    print('It is work!')
+
+
+if __name__ == '__main__':
+    main()
