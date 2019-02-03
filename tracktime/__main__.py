0e6af47ac7fbd83dbb2f05553d11ab90c290c199
Damir Mingazov <damirm@list.ru>
2019-02-03 17:04:36 +0400

    Add setting through the environment.

--- tracktime/__main__.py
+++ tracktime/__main__.py
@@ -2,16 +2,27 @@
 #
 # An application that allows you to entry time in the Redmine through a bot
 # in Telegram
-"""An application that allows you to entry time in the Redmine through a bot."""
+"""An application that allows you to entry time in the Redmine through a bot.
+
+The application must be configured through the environment.
+
+Environment:
+    TELEGRAM_TOKEN : The telegram token.
+    REDMINE_URL : Redmine URI that should track time entry.
+    DSN_DB: Optional. Default `sqlite:///sqlite.db`. Database data source name.
+    PROXY_URL: Optional. URI proxy through which the bot will work.
+    PROXY_USERNAME: Optional. Proxy username.
+    PROXY_PASSWORD: Optional. Proxy password.
+"""
 
 import logging
+import os
 
 from sqlalchemy import create_engine
 from telegram import Bot, Update
 from telegram.ext import Updater
 
-from tracktime.bot import create_help_handler, create_setting_handler, \
-    create_tracktime_handler
+from tracktime.bot import create_help_handler, create_setting_handler, create_tracktime_handler
 from tracktime.models import initialize_tables
 
 
@@ -22,14 +33,36 @@ def __error(bot: Bot, update: Update, error):
 
 
 def run(config: dict):
-    """Run the application."""
-    updater = Updater(config['token'], workers=1, request_kwargs={
-        'proxy_url': config['proxy_url'],
-        'urllib3_proxy_kwargs': {
-            'username': config['proxy_username'],
-            'password': config['proxy_password'],
-        },
-    })
+    """Run the application.
+
+    :param config: Configuration dictionary.
+
+    Key `proxy` is optional. If key `proxy` is not exists then bot create
+    a standard connection. Example:
+
+        config = {
+            'token': 'TELEGRAM_TOKEN',
+            'redmine_url': 'REDMINE_URL',
+            'dsn_db': 'DSN_DB',
+            'proxy': {  # Optional
+                'url': 'PROXY_URL',
+                'username': 'PROXY_USERNAME',
+                'password': 'PROXY_PASSWORD'
+            }
+        }
+
+    """
+    request_kwargs = {}
+    if 'proxy' in config:
+        request_kwargs = {
+            'proxy_url': config['proxy']['url'],
+            'urllib3_proxy_kwargs': {
+                'username': config['proxy']['username'],
+                'password': config['proxy']['password'],
+            }
+        }
+
+    updater = Updater(config['token'], workers=1, request_kwargs=request_kwargs)
 
     engine = create_engine(config['dsn_db'], echo=True)
     initialize_tables(engine)
@@ -39,8 +72,7 @@ def run(config: dict):
                                              redmine_url=config['redmine_url'])
     tracktime_handler = create_tracktime_handler(engine=engine,
                                                  job_queue=updater.job_queue,
-                                                 redmine_url=config[
-                                                     'redmine_url'],
+                                                 redmine_url=config['redmine_url'],
                                                  start_command_name='track',
                                                  cancel_command_name='cancel')
     help_handler = create_help_handler(command_name='help')
@@ -55,13 +87,29 @@ def run(config: dict):
     updater.idle()
 
 
-if __name__ == '__main__':
+def __get_env_config():
     config = {
-        'token': 'TOKEN',
-        'proxy_url': 'socks5://proxy',
-        'proxy_username': 'proxy_username',
-        'proxy_password': 'proxy_password',
-        'redmine_url': 'https://host_redmine',
-        'dsn_db': 'sqlite:///sqlite.db',
+        'token': os.environ['TELEGRAM_TOKEN'],
+        'redmine_url': os.environ['REDMINE_URL'],
+        'dsn_db': os.getenv('DSN_DB', 'sqlite:///sqlite.db')
     }
+
+    config_proxy = {}
+    if 'PROXY_URL' in os.environ:
+        config_proxy['url'] = os.getenv('PROXY_URL')
+
+    if 'PROXY_USERNAME' in os.environ:
+        config_proxy['username'] = os.getenv('PROXY_USERNAME')
+
+    if 'PROXY_PASSWORD' in os.environ:
+        config_proxy['password'] = os.getenv('PROXY_PASSWORD')
+
+    if len(config_proxy) == 3:
+        config['proxy'] = config_proxy
+
+    return config
+
+
+if __name__ == '__main__':
+    config = __get_env_config()
     run(config)
